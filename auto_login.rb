723cc759638b3c7180c02b4440e665311b7ac5ea
James-Hopkins <james@hopkins.irish>
2024-10-13 22:47:41 +0100

    working with multiple threads

--- auto_login.rb
+++ auto_login.rb
@@ -10,50 +10,61 @@ config = {
     meeting_url: config_data['meeting_url']
 }
 
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--use-fake-device-for-media-stream')
-options.add_argument('--use-fake-ui-for-media-stream')
+def build_driver
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--use-fake-device-for-media-stream')
+    options.add_argument('--use-fake-ui-for-media-stream')
 
-driver = Selenium::WebDriver.for :chrome, options: options
+    Selenium::WebDriver.for :chrome, options: options
+end
 
-driver.navigate.to 'https://account.microsoft.com/account/manage-my-account'
+def login_join_and_leave_meeting(user, meeting_url, driver)
+    driver.navigate.to 'https://account.microsoft.com/account/manage-my-account'
 
-# Find and interact with elements (e.g., login)
-driver.find_element(:id, 'id__5').click
-# wait for a specific element to show up
-wait = Selenium::WebDriver::Wait.new(timeout: 10) # seconds
-wait.until { driver.find_element(name: 'loginfmt') }
-driver.find_element(:name, 'loginfmt').send_keys(config[:users][0][:email])
-driver.find_element(:id, 'idSIButton9').click
+    driver.find_element(:id, 'id__5').click
+    wait = Selenium::WebDriver::Wait.new(timeout: 10) # seconds
+    wait.until { driver.find_element(name: 'loginfmt') }
+    driver.find_element(:name, 'loginfmt').send_keys(user[:email])
+    driver.find_element(:id, 'idSIButton9').click
 
-sleep 1
+    sleep 1 # these are added because the elements are not yet enteraable so the built in wait fails
 
-wait.until { driver.find_element(id: 'i0118', name: 'passwd') }
-driver.find_element(id: 'i0118', name: 'passwd').send_keys(config[:users][0][:password])
+    wait.until { driver.find_element(id: 'i0118', name: 'passwd') }
+    driver.find_element(id: 'i0118', name: 'passwd').send_keys(user[:password])
 
-sleep 1
+    sleep 1
 
-driver.find_element(:id, 'idSIButton9').click
-wait.until { driver.find_element(id: 'declineButton') }
-driver.find_element(id: 'declineButton').click
+    driver.find_element(:id, 'idSIButton9').click
+    wait.until { driver.find_element(id: 'declineButton') }
+    driver.find_element(id: 'declineButton').click
 
-sleep 1
+    sleep 1
 
-driver.navigate.to config[:meeting_url]
-wait.until { driver.find_element(xpath: '//*[@id="container"]/div/div/div[1]/div[4]/div/button[1]/div/h3') }
-driver.find_element(xpath: '//*[@id="container"]/div/div/div[1]/div[4]/div/button[1]/div/h3').click
+    driver.navigate.to meeting_url
+    wait.until { driver.find_element(xpath: '//*[@id="container"]/div/div/div[1]/div[4]/div/button[1]/div/h3') }
+    driver.find_element(xpath: '//*[@id="container"]/div/div/div[1]/div[4]/div/button[1]/div/h3').click
 
-sleep 1
+    sleep 1
 
-wait.until { driver.find_element(xpath: '//*[@id="prejoin-join-button"]') }
-driver.find_element(xpath: '//*[@id="prejoin-join-button"]').click
+    wait.until { driver.find_element(xpath: '//*[@id="prejoin-join-button"]') }
+    driver.find_element(xpath: '//*[@id="prejoin-join-button"]').click
 
-sleep 15 # loading the ms teams web app takes a while
+    sleep 15 # loading the ms teams web app takes a while
 
-wait.until { driver.find_element(xpath: '//*[@id="app"]/div/div/div/div[4]/div[1]/div/div/div/div/div[3]') }
-driver.find_element(xpath: '//*[@id="app"]/div/div/div/div[4]/div[1]/div/div/div/div/div[3]').click
+    wait.until { driver.find_element(xpath: '//*[@id="app"]/div/div/div/div[4]/div[1]/div/div/div/div/div[3]') }
+    driver.find_element(xpath: '//*[@id="app"]/div/div/div/div[4]/div[1]/div/div/div/div/div[3]').click
 
-sleep 30
+    sleep 30
 
-# Close the browser session when done
-driver.quit
+    driver.quit # Close the browser session when done
+end
+
+# Use threads to handle multiple users concurrently
+threads = config[:users].map do |user|
+    Thread.new do
+        login_join_and_leave_meeting(user, config[:meeting_url], build_driver)
+    end
+end
+
+# Wait for all threads to complete
+threads.each(&:join)
